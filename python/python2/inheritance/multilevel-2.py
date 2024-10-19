c1e1d0b2e68f96baffd7b271a98c1501e4bcee7b
Mittal2801 <mituprajapati2801@gmail.com>
2024-10-19 11:58:42 +0530

    multilevel-2 add

--- python/python2/inheritance/multilevel-2.py
+++ python/python2/inheritance/multilevel-2.py
@@ -1,2 +1,25 @@
 class Person:
-    def __init__(s)
+    def __init__(self):
+        print('Person - Hii')
+    def age(self,a):
+        print('Printing the age:',a)
+        
+class Father(Person):
+    def __init__(self):
+        print('Father - Hii')
+        super().__init__()
+    def age(self,a):
+        print('Printing the age(Father):',a)
+        super().age(a+21)
+        
+class Mother(Father):
+    def __init__(self):
+        print('Mother - Hii')
+        super().__init__()
+    def age(self,a):
+        print('Printing the age(Mother):',a)
+        super().age(a+5)
+        
+#Main function
+o = Mother()
+o.age(30)
