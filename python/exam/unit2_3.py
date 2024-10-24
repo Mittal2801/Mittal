6c3a16094e1b1b70ba0145def4206f297bea74a0
Mittal2801 <mituprajapati2801@gmail.com>
2024-10-24 08:38:23 +0530

    unit 2 3

---
+++ python/exam/unit2_3.py
@@ -0,0 +1,22 @@
+a = [1,2,3,4,5,5,5,3,4,5,5,5]
+#append
+a.append(6)#print ma na thaay
+print(a)
+
+#insert
+a.insert(2,25)
+print(a)
+
+#remove
+a.remove(4)#je value delete karvi hoy te nakhvi
+print(a)
+
+#pop
+a.pop()
+print(a)
+
+#index
+print(a.index(2))#enter karel value ni index sodhva... list ma na hoy to error through kare
+
+#count
+print(a.count(5))#koi bi vastu count karva mate
