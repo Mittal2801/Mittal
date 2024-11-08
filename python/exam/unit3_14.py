15dcabf41b1458f45fe734337fcef6abd5278353
Mittal2801 <mituprajapati2801@gmail.com>
2024-11-08 21:39:35 +0530

    add mro

---
+++ python/exam/unit3_14.py
@@ -0,0 +1,33 @@
+class A(object):
+    def method(self):
+        print("A class method")
+        super().method()
+        
+class B(object):
+    def method(self):
+        print("B class method")
+        super().method()
+        
+class C(object):
+    def method(self):
+        print("C class method")
+        super().method()
+        
+class X(A,B):
+    def method(self):
+        print("X class method")
+        super().method()
+        
+class Y(B,C):
+    def method(self):
+        print("Y class method")
+        super().method()
+        
+class P(X,Y,C):
+    def method(self):
+        print("P class method")
+        super().method()
+        
+newp = P()
+print(P.mro())
+newp.method()
