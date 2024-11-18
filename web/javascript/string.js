42f11e54a35ac72cd659360ef07bc622a5429f96
Mittal2801 <mituprajapati2801@gmail.com>
2024-11-18 18:11:13 +0530

    add string

---
+++ web/javascript/string.js
@@ -0,0 +1,22 @@
+let str = "Talent Computer Academy";
+console.log(str); // print string
+console.log(str.length); // length of string
+
+console.log(str[3]); // print index word
+
+console.log(str.toUpperCase()) //write string in uppercase
+console.log(str.toLowerCase()) // write string in lowercase
+console.log(str.trim()) // removes whitespaces
+
+// str.slice(start,end)
+console.log(str.slice(3));
+console.log(str.slice(3,11));
+
+s1 = "talent 1"
+s2 = "Computer"
+console.log(s1.concat(s2));
+
+// str.replace(searchVal,newVal);
+console.log(str.replace("Computer",'TCIT'));
+
+
