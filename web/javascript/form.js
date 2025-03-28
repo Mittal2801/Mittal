8a11c069768c937204a0488723b24ad2f56386df
Mittal2801 <mituprajapati2801@gmail.com>
2025-03-28 17:37:02 +0530

    add form

---
+++ web/javascript/form.js
@@ -0,0 +1,63 @@
+document.getElementById("loginform").addEventListener("submit",function(event){     
+            
+    event.preventDefault();
+    
+    let username = document.getElementById("username").value.trim();
+    let usernameError = document.getElementById("usernameError");
+    if (username.trim() === "") {
+        usernameError.textContent = "Username is required";
+        isValid = false;
+    } 
+    else {
+        usernameError.textContent = "";
+    }
+
+    let email = document.getElementById("email").value.trim();
+    let emailError = document.getElementById("emailError");
+    if (email.trim() === "") {
+        emailError.textContent = "Email is required";
+        isValid = false;
+    }
+    else {
+        emailError.textContent = "";
+    }
+
+    let password = document.getElementById("password").value.trim();
+    let passwordError = document.getElementById("passwordError");
+
+    if(password.length < 6) {
+        passwordError.textContent = "Password must be 6 later";
+        isValid = false;
+    }
+    else {
+        passwordError.textContent = "";
+    }
+
+    let phone = document.getElementById("phone").value.trim();
+    let phoneError = document.getElementById("phoneError");
+
+    if (phone.length < 10) {
+        phoneError.textContent = "Mobile number must be 10 numbers";
+        isValid = false;
+    }
+    else {
+        phoneError.textContent = "";
+    }
+
+    let age = document.getElementById("age").value.trim();
+    let ageError = document.getElementById("ageError");
+
+    if (age < 18 && age > 40 ) {
+        ageError.textContent = "Not valid age";
+        isValid = false;
+    }
+    else {
+        ageError.textContent = "";
+    }
+
+
+    if (isValid) {
+        alert("Form submitted successfully!!");
+    }
+
+});
