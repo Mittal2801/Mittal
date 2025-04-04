6524b8ae428e6a73cbecc466595f7d36dfed5f81
Mittal2801 <mituprajapati2801@gmail.com>
2025-04-04 18:51:57 +0530

    add today work

--- web/javascript/form.js
+++ web/javascript/form.js
@@ -61,3 +61,47 @@ document.getElementById("loginform").addEventListener("submit",function(event){
     }
 
 });
+
+let UpperCase = false;
+let LowerCase = false;
+let Number = false;
+let SpecialChar = false;
+let Length = false;
+
+for (let i = 0; i < password.length; i++) {
+    let char = password[i];
+
+    if(password.length <= 8) {
+        Length = true;
+    }
+
+    if (char >= 'A' && char <= 'Z') {
+        hasUpperCase = true;
+        
+    }
+
+    if (char >= 'a' && char <= 'z') {
+        hasLowerCase = true;
+    }
+
+    if (char >= '0' && char <= '9') {
+        hasNumber = true;
+    }
+
+    if (!(char >= 'A' && char <= 'Z') && !(char >= 'a' && char <= 'z') && !(char >= '0' && char <= '9')) {
+        hasSpecialChar = true;
+    }
+}
+if (UpperCase && LowerCase && Number && SpecialChar && Length) {
+    passwordError.textContent = "";
+} 
+else if (UpperCase || LowerCase || !Number || !SpecialChar || !Length) {
+    passwordError.textContent ="Password must contain at least one uppercase letter.";
+    passwordError.textContent ="Password must contain at least one lowercase letter.";
+    passwordError.textContent ="Password must contain at least number.";
+    passwordError.textContent ="Password must contain at least one special character.";
+    passwordError.textContent ="Password must contain at least eight character.";
+}
+else {
+    passwordError.textContent ="Password must contain at least one uppercase letter, one lowercase letter, one number,one special character,and password must be 8 character.";
+}
