10959838e5395f0b71fa7168cfa3801949cfb943
Mittal2801 <mituprajapati2801@gmail.com>
2025-05-13 18:07:04 +0530

    add

--- web/javascript/arrray/arraymethod.js
+++ web/javascript/arrray/arraymethod.js
@@ -4,9 +4,13 @@ function arrayMethodExample() {
     let squaredNumbers = numbers.map(num => num * num);     //Map modified
     let EvenNumbers = numbers.filter(num => num%2 === 0);   //Filter modified
     let sum = numbers.reduce((acc,num) => acc+num,0);       //Reduce modified
+    let multi = numbers.reduce((acc,num) => acc*num,1);       //Reduce modified
+
 
     document.write("Squared numbers: "+squaredNumbers.join(", ")+"<br>");
     document.write("Even numbers: "+EvenNumbers.join(", ")+"<br>");
     document.write("Sum of numbers: "+sum+"<br>");
+    document.write("Multiplication of numbers: "+multi+"<br>");
+
 }
 arrayMethodExample()
